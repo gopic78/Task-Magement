1f3e20848a28209d50fb6a3f5ced58efa6db6f5a
Gopi C <140308144+gopic78@users.noreply.github.com>
2024-08-12 22:09:05 +0530

    app commit

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.get('/',(req,res)=>{
     res.status(200).json("The page is render")
 })
 app.get('/author',(req,res)=>{
-    res.status(200).json({name : "Gopi C", github : "git remote add origin https://github.com/gopic78/Task-Magement.git"})
+    res.status(200).json({name : "Gopi C", github : "https://github.com/gopic78/Task-Magement.git"})
 })
 
 app.use((req, res, next) => {
